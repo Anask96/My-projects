57b21507ca760d56ced2350293a03a2fedfe7212
Anask96 <85752013+Anask96@users.noreply.github.com>
2023-11-15 12:49:15 +0100

    Changes

--- C++/BinarySearchTree.cpp
+++ C++/BinarySearchTree.cpp
@@ -183,8 +183,8 @@ void BST::prettyPrint(){
   
 int main() {
   
-  BST tree; 
-  std::vector<int> vect; 
+  BST tree; //Dont erase this 
+  std::vector<int> vect; //change this
   vect.push_back(0);
   vect.push_back(4);
   vect.push_back(-4);
